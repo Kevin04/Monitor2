6fab4f42fec4011d3f0b8f694e0435d8a84b1de1
josanuz <josanuz@hotmail.es>
2014-10-20 23:44:45 -0600

    Modify User Done

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,20 +10,18 @@ import java.io.File;
 import java.net.URL;
 
 public class Main extends Application {
-
     public static String screen1ID = "main";
     public static String screen1File = "/sample/View/Login.fxml";
     public static String screen2ID = "screen2";
     public static String screen2File = "/sample/View/VistaPrincipal.fxml";
-    public static String autoLogin="autoLogin";
+    public static String autoLogin = "autoLogin";
     public static String autoLoginFile = "/sample/View/AutoLogin.fxml";
-    public static String RoleNPrivileges="rolesNprivileges";
+    public static String RoleNPrivileges = "rolesNprivileges";
     public static String RoleNPrivilegesFile = "/sample/View/VentanaPrivilegios.fxml";
     public static ScreensController mainContainer;
-    public static String ModifyRoleNPrivileges="ControllerModifyRolesAndPrivileges";
+    public static String ModifyRoleNPrivileges = "ControllerModifyRolesAndPrivileges";
     public static String ModifyRoleNPrivilegesFile = "/sample/View/ViewModifyPrivileges.fxml";
-
-    public static final String keyEncrypth="Mary has one ca1";
+    public static final String keyEncrypth = "Mary has one ca1";
     public static String appWindowFile = "/sample/View//Main.fxml";
     public static String appWindow = "appWindow";
     public static String creteUserWindow = "createUserWindow";
@@ -31,10 +29,8 @@ public class Main extends Application {
     public static String modifyUserWindow = "modifiUserWindow";
     public static String modifyUserFile = "/sample/View/ModifyUser.fxml";
 
-
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         URL u2 = getClass().getResource("/sample/View/Login.fxml");
         File theDir = new File("src");
         if (!theDir.exists()) {
@@ -61,10 +57,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
-
-
 }
